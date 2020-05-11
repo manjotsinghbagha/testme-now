d903eeba1a1a4ab91c01430a8bf320306a6fc162
devel-jot <54830219+manjotsinghbagha@users.noreply.github.com>
2020-05-11 21:07:11 +0000

    Update settings.py

--- testme/settings.py
+++ testme/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path .abspath(__file__)))
 SECRET_KEY = '-u#h$wzb9vkxd9=7n_&=&g)&u&yji0f#l&75=mtnsbx3cbo!^1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False 
 
 ALLOWED_HOSTS = ['*']
 
